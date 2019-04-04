5fce95c859f20d8a44d9dd6ab512cc5d36383fd8
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-04-04 21:33:24 +0900

    Complete draft google authentication

--- client/src/components/templates/SignInTemplate/index.tsx
+++ client/src/components/templates/SignInTemplate/index.tsx
@@ -1,22 +1,43 @@
-import React, { Fragment } from 'react'
-import Button from '../../atoms/Buttons/SNSSigiInButton'
+import React, { Fragment, useState, useEffect } from 'react'
+import firebase from '../../../firebase'
+import SNSSignInButton from '../../atoms/Buttons/SNSSigiInButton'
 
 interface Props {
   onClick: () => void
 }
 
-const SignInTemplate: React.FC<Props> = ({ onClick }) => (
-  <Fragment>
-    <Button type="google" onClick={onClick}>
-      Login with Google
-    </Button>
-    <Button type="twitter" onClick={onClick}>
-      Login with Twitter
-    </Button>
-    <Button type="facebook" onClick={onClick}>
-      Login with Facebook
-    </Button>
-  </Fragment>
-)
+type User = firebase.User | null
+
+const onClickGoogleLogin = () => {
+  const provider = new firebase.auth.GoogleAuthProvider()
+  firebase.auth().signInWithRedirect(provider)
+}
+
+const SignInTemplate: React.FC<Props> = ({ onClick }) => {
+  const [currentUser, setCurrentUser] = useState<User>(null)
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      setCurrentUser(user)
+    })
+  })
+
+  return !currentUser ? (
+    <Fragment>
+      <SNSSignInButton type="google" onClick={onClickGoogleLogin}>
+        Login with Google
+      </SNSSignInButton>
+      <SNSSignInButton type="twitter" onClick={onClick}>
+        Login with Twitter
+      </SNSSignInButton>
+      <SNSSignInButton type="facebook" onClick={onClick}>
+        Login with Facebook
+      </SNSSignInButton>
+    </Fragment>
+  ) : (
+    <Fragment>
+      <p>Hello, {currentUser.email}👋</p>
+    </Fragment>
+  )
+}
 
 export default SignInTemplate
