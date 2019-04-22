0f9c9f8fcfeabeeed2731e3ba6d08e1788cc77e1
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-04-22 12:08:47 +0900

    Fix plugin types

--- src/components/organisms/MarkdownEditor/index.tsx
+++ src/components/organisms/MarkdownEditor/index.tsx
@@ -8,17 +8,38 @@ import InlineStyleControls from '../../molecules/TypeControls/InlineStyleControl
 import { STYLE_MAP } from '../../../constants/MarkdownEditor/editor_style'
 import * as styles from './style.css'
 
+// TODO: 型付け厳格に。ただ、そもそもmoduleの型付けの方を厳格化すべきかも。
 interface Plugin {
-  blockRenderMap: Map<string, string>
-  blockRendererFn: (block: string, {}) => {} | null
-  blockStyleFn: (block: string) => object | null
-  decorators: object[]
-  handleBeforeInput: (ev: string, html: HTMLElement, setEditorState: EditorState, {}) => string
-  handleKeyCommand: (command: string, setEditorState: EditorState, {}) => string
-  handlePastedText: (ev: string, setEditorState: EditorState, {}) => string
-  handleReturn: (ev: string, setEditorState: EditorState, {}) => string
-  initialize: (setEditorState: EditorState, getEditState: EditorState) => void
-  onTab: (ev: string, {}) => string
+  blockRenderMap: Map<string, any>
+  blockRendererFn: (
+    block: Record<string, any>,
+    args: Record<string, Function>
+  ) => Record<string, any>
+  blockStyleFn: (block: Record<string, any>) => 'checkable-list-item' | null
+  decorators: Function[]
+  handleBeforeInput: (
+    character: any,
+    editorState: EditorState,
+    args: Record<'setEditorState', Function>
+  ) => string
+  handleKeyCommand: (
+    command: string,
+    editorState: EditorState,
+    args: Record<'setEditorState', Function>
+  ) => string
+  handlePastedText: (
+    text: string,
+    html: any,
+    editorState: EditorState,
+    args: Record<'setEditorState', Function>
+  ) => string
+  handleReturn: (
+    ev: any,
+    editorState: EditorState,
+    args: Record<'setEditorState', Function>
+  ) => string
+  initialize: (args: Record<'getEditorState' | 'setEditorState', Function>) => void
+  onTab: (ev: string, args: Record<'getEditorState' | 'setEditorState', Function>) => string
   store: {}
 }
 
