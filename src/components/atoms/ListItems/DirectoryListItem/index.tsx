ce581639141d5012489be03c9a06f0e0e7d4caa3
Shota Yamazaki <sysy5867@gmail.com>
2019-05-03 14:55:06 +0900

    remove unnecessary code

--- src/components/atoms/ListItems/DirectoryListItem/index.tsx
+++ src/components/atoms/ListItems/DirectoryListItem/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { withStyles } from '@material-ui/core/styles'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
@@ -10,26 +8,15 @@ import { faFolderOpen } from '@fortawesome/free-solid-svg-icons'
 interface Props {
   id: string
   label: string
-  classes: any
 }
-
-const styles = () => ({
-  root: {
-    border: 'none',
-  },
-})
 
 const iconFolder = <FontAwesomeIcon icon={faFolderOpen} />
 
-const DirectoryListItem: React.FC<Props> = ({ id, label, classes }) => (
-  <ListItem className={classes.root} button component="a" href={`/${id}`}>
+const DirectoryListItem: React.FC<Props> = ({ id, label }) => (
+  <ListItem button component="a" href={`/${id}`}>
     <ListItemIcon>{iconFolder}</ListItemIcon>
     <ListItemText primary={label} />
   </ListItem>
 )
 
-DirectoryListItem.propTypes = {
-  classes: PropTypes.object.isRequired,
-}
-
-export default withStyles(styles)(DirectoryListItem)
+export default DirectoryListItem
