1cb773ea78be79574851c971ad7bbde48e8def81
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-05-06 10:15:17 +0900

    Enable to convert blocks to text

--- src/actions/commits.ts
+++ src/actions/commits.ts
@@ -1,6 +1,6 @@
 import { db, FirebaseSnapShot } from '../utils/firebase'
 import { ThunkDispatch } from 'redux-thunk'
-// import { RawDraftContentBlock } from 'draft-js'
+import { RawDraftContentBlock } from 'draft-js'
 import { actionTypes } from '../constants'
 import { BaseAction, FirebaseAPIRequest, FirebaseAPIFailure } from './static-types'
 import { ReduxAPIStruct } from '../reducers/static-types'
@@ -20,14 +20,65 @@ interface CreateCommitAction extends BaseAction {
 
 export type CommitsAction = FirebaseAPIRequest | FirebaseAPIFailure | CreateCommitAction
 
+const convertToText = (rawContentBlocks: RawDraftContentBlock[]) => {
+  let commitBody = ''
+  for (const block of rawContentBlocks) {
+    let inlineStyledBlockText = block.text
+    block.inlineStyleRanges.forEach((inlineStyle, index) => {
+      let start = inlineStyle.offset
+      const length = inlineStyle.length
+      const insertInlineChar = (text: string, char: string) => {
+        start += index * 2 // 前回までのループで加算されたcharの数を加味
+        const end = start + length
+        return text.slice(0, start) + char + text.slice(start, end) + char + text.slice(end)
+      }
+
+      switch (inlineStyle.style) {
+        case 'BOLD':
+          inlineStyledBlockText = insertInlineChar(inlineStyledBlockText, '*')
+          break
+
+        case 'ITALIC':
+          inlineStyledBlockText = insertInlineChar(inlineStyledBlockText, '_')
+          break
+      }
+    })
+
+    switch (block.type) {
+      case 'unstyled':
+        commitBody += `${inlineStyledBlockText}\n`
+        break
+
+      case 'header-one':
+        commitBody += `# ${inlineStyledBlockText}\n`
+        break
+
+      case 'header-two':
+        commitBody += `## ${inlineStyledBlockText}\n`
+        break
+
+      case 'blockquote':
+        commitBody += `> ${inlineStyledBlockText}\n`
+        break
+
+      case 'unordered-list-item':
+        commitBody += `- ${inlineStyledBlockText}\n`
+        break
+    }
+  }
+
+  return commitBody
+}
+
 export const createCommit = (
   values: any,
   currentUserUid: string,
   directoryId: string,
-  branchId: string
-  // rawContentBlocks: RawDraftContentBlock[]
+  branchId: string,
+  rawContentBlocks: RawDraftContentBlock[]
 ) => {
   return (dispatch: ThunkDispatch<{}, {}, CommitsAction>) => {
+    const commitText = convertToText(rawContentBlocks)
     dispatch({ type: actionTypes.COMMIT__FIREBASE_REQUEST })
     db.collection('users')
       .doc(currentUserUid)
@@ -38,7 +89,7 @@ export const createCommit = (
       .collection('commits')
       .add({
         name: values.commitName,
-        body: '',
+        body: commitText,
         cratedAt: Date.now(),
       })
       .then(newDocRef => {
