c370efee579e576db351f91c674dd7fe18364ce1
Shota Yamazaki <sysy5867@gmail.com>
2019-05-27 09:26:44 +0900

    add min style

--- src/components/organisms/DisclaimerMessage/index.tsx
+++ src/components/organisms/DisclaimerMessage/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { disclaimerMessageWrapper, disclaimerMessage } from './style.css'
 
 const DisclaimerMessage: React.FC = () => (
-  <p>
-    Elaborateをご利用頂きありがとうございます。本サービスは現在、スマートフォン、タブレットによるご利用に対応しておりません。お手数ですが、PCでのご利用をお願いいたします。
-  </p>
+  <div className={disclaimerMessageWrapper}>
+    <p className={disclaimerMessage}>
+      Elaborateをご利用頂きありがとうございます。本サービスは現在、スマートフォン、タブレットによるご利用に対応しておりません。お手数ですが、PCでのご利用をお願いいたします。
+    </p>
+  </div>
 )
 
 export default DisclaimerMessage
