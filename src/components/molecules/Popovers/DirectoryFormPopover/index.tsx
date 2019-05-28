523f6a32ea6139db7be36377c87bec5e676eab66
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-05-28 12:57:57 +0900

    Popoverize directory form

--- src/components/molecules/Popovers/DirectoryFormPopover/index.tsx
+++ src/components/molecules/Popovers/DirectoryFormPopover/index.tsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import Popover from '@material-ui/core/Popover'
-import BranchForm from '../../Forms/BranchForm'
-import { ReduxAPIStruct } from '../../../../common/static-types/api-struct'
-import { FirebaseSnapShot } from '../../../../utils/firebase'
+import DirectoryForm from '../../Forms/DirectoryForm'
 
 interface Props {
   currentUser: firebase.User
-  directoryId: string
-  branches: ReduxAPIStruct<FirebaseSnapShot[]>
   isOpen: boolean
   anchorElement: EventTarget & HTMLDivElement | null
   handleClose: () => void
 }
 
-const BranchFormPopover: React.FC<Props> = ({ isOpen, anchorElement, ...rest }) => (
+const BranchFormPopover: React.FC<Props> = ({
+  currentUser,
+  isOpen,
+  anchorElement,
+  handleClose,
+}) => (
   <Popover
     open={isOpen}
     anchorEl={anchorElement}
-    onClose={rest.handleClose}
+    onClose={handleClose}
     transformOrigin={{
       vertical: 'bottom',
       horizontal: 'right',
@@ -27,7 +28,7 @@ const BranchFormPopover: React.FC<Props> = ({ isOpen, anchorElement, ...rest }) 
       horizontal: 'right',
     }}
   >
-    <BranchForm {...rest} />
+    <DirectoryForm currentUser={currentUser} />
   </Popover>
 )
 
