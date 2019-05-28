80f7f408223ac58b46523278ec71a17ac6494bf3
Shota Yamazaki <sysy5867@gmail.com>
2019-05-28 09:25:27 +0900

    スマートフォンからの流入の警告のデザインを良い感じに

--- src/components/organisms/DisclaimerMessage/index.tsx
+++ src/components/organisms/DisclaimerMessage/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import { disclaimerMessageWrapper, disclaimerMessage } from './style.css'
+import imgUrl from '../../../img/pc.png'
+import * as styles from './style.css'
+
+const { disclaimerMessageWrapper, logo, pc, pcWrapper, disclaimerMessage } = styles
 
 const DisclaimerMessage: React.FC = () => (
   <div className={disclaimerMessageWrapper}>
-    <p className={disclaimerMessage}>
-      Elaborateをご利用頂きありがとうございます。本サービスは現在、スマートフォン、タブレットによるご利用に対応しておりません。お手数ですが、PCでのご利用をお願いいたします。
-    </p>
+    <h1 className={logo}>Elaborate</h1>
+    <div className={pcWrapper}>
+      <img className={pc} src={imgUrl} />
+    </div>
+    <div>
+      <p className={disclaimerMessage}>
+        Elaborate is not offered on smartphones and tablets at a current version.
+      </p>
+      <p className={disclaimerMessage}>
+        Go to elabor-8.com on a PC to start “elaborate” your doucuments.
+      </p>
+    </div>
   </div>
 )
 
