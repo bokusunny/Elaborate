e3111254e767718e8477866fd999ac2c1f718bc4
Shota Yamazaki <sysy5867@gmail.com>
2019-06-09 16:21:19 +0900

    update: LPの文言を修正

--- src/components/molecules/LandingMessage/index.tsx
+++ src/components/molecules/LandingMessage/index.tsx
@@ -3,11 +3,11 @@ import * as styles from './style.css'
 
 // MEMO: 本来moleculesではデータを持たないが、このコンポーネントは他に使いどころがないので静的情報をコンポーネント内で保持。(YMZK)
 
-const title = 'Creative work space\n to elaborate your ideas.'
+const title = 'A creative work space\n to elaborate your ideas.'
 const message = `
-Who’s heart do you want to move by your words?\n
-All you need is just start writing and think through until having it enough.\n
-We offer a simply designed editor and a version controlling system.\n
+Whose heart do you want to move with your words?\n
+All you need to do is start writing!\n
+We offer a streamlined editor and version control system.\n
 `
 
 const { messageWrapper, catchCopy, catchCopyText, description, descriptionText } = styles
